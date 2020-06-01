8f1263ad14c504a9f74f6a4246391a2ec7f68cc3
Ramdane <ramdane_kial@live.fr>
2020-06-01 15:34:51 +0000

    creation script d'installation

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install python3-pip
+sudo pip3 install Flask
+cd /tmp
+sudo curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
+sudo bash Anaconda3-2019.03-Linux-x86_64.sh
+sudo source ~/.bashrc
